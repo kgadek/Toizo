9a8eda9c6c8ce59d38e1cf2bfba6cf8ffdabd2e2
Konrad Gądek <kgadek@gmail.com>
2012-09-03 19:44:33 +0200

    Implementacja union-set z rollback
    Nie jest idealne, ale dla tego programu wystarczy.
    Uwaga na przypadki brzegowe!

--- t.cpp
+++ t.cpp
@@ -4,10 +4,13 @@
 #include <cstdlib>
 
 typedef unsigned int uint;
+typedef unsigned char uchar;
 
 struct node {
 	char type; //!< Typ obiektu: A=0, B=1, ...
 	char rot; //!< Obrót obiektu: 0 = bez obrotu, 1 = obrót o 90 stopni ("clockwise").
+	node* p; //!< Dla union-findSet: pointer na ojca.
+	uint s; //!< Dla union-findSet: rozmiar poddrzewa.
 };
 char rotations[] = {1,2,4,1,2,4,4,4,4}; //!< Ilość obrotów kolejnych typów klocków (np. 'A' jest symetryczny, ma jeden wartościowy obrót).
 
@@ -24,6 +27,11 @@ node *brd; //!< brd = (*board).
 void read_board();
 void print_board();
 
+node** rollbackHeap;
+uint rollbackHeapHead = 0;
+node* findSet(node*);
+void commitOrRollback(const bool);
+void unionSet(node* const, node* const);
 
 /**
  * Główna metoda.
@@ -54,16 +62,22 @@ void read_board() {
 	char input[INPUTLEN];
 	scanf(" %d %d ", &X, &Y);
 	XY = X*Y;
+	rollbackHeap = (node**)malloc(2*XY*sizeof(node*));
 
 	// board[x][y] == (*board)[i] == brd[i]   dla   i == X*y + x
 	board = (node**)malloc(Y*sizeof(node*));
 	brd = (*board) = (node*)malloc(XY*sizeof(node));
 	for(uint i=0;i<Y;++i) board[i] = *board + i*X;
+	for(uint i=0;i<XY;++i, ++brd) { // ustawianie .p .s
+		(*brd).p = brd;
+		(*brd).s = 1;
+	}
+	brd = (*board);
 
 	uint board_pos = 0;
 	char *inp;
 	while(fgets(input,INPUTLEN,stdin) != NULL) {
-		for(inp=input; *inp; inp+=3, ++board_pos) {
+		for(inp=input; *inp; inp+=3, ++board_pos) { // ustawianie .type .rot
 			if((*inp) == 'H' || ('D' <= (*inp) && (*inp) <= 'F'))
 				batt_pos = board_pos;
 			brd[board_pos].type = (*inp) - 'A';
@@ -89,3 +103,55 @@ void print_board() {
 		printf("\n");
 	}
 }
+
+
+/**
+ * Wyszukiwanie reprezentanta zbioru.
+ *
+ * Używamy heurezy łączenia wg rozmiarów, więc koszt czasowy to O(log n).
+ */
+node* findSet(node *n) {
+	while(n->p != n) n = n->p;
+	return n;
+}
+
+
+/**
+ * Commit na zbiorach.  
+ */
+void commitSet() {
+	if(rollbackHeapHead == 0 || rollbackHeap[rollbackHeapHead-1] != 0)
+		rollbackHeap[rollbackHeapHead++] = 0;
+}
+
+
+/**
+ * Rollback do ostatniego commita (na zbiorach).
+ */
+void rollbackSet() {
+	if(rollbackHeap[rollbackHeapHead-1] == 0 && rollbackHeapHead > 0)
+		--rollbackHeapHead;
+	while(rollbackHeapHead > 0 && rollbackHeap[--rollbackHeapHead] != 0) {
+		node *n = rollbackHeap[rollbackHeapHead];
+		n->p->s -= n->s;
+		n->p = n;
+	}
+}
+
+
+/**
+ * Operacja union. 
+ */
+void unionSet(node* n, node* m) {
+	if(findSet(n) == findSet(m)) return;
+	if(n->s < m->s) {
+		n->p = m;
+		m->s += n->s;
+		rollbackHeap[rollbackHeapHead++] = n;
+	} else {
+		m->p = n;
+		n->s += m->s;
+		rollbackHeap[rollbackHeapHead++] = m;
+	}
+}
+
